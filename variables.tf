bbde9faded0fd3786002c68b6622922a94def467
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:40:45 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,60 @@
+variable "data_factory_trigger_schedules" {
+  description = <<EOT
+Map of data_factory_trigger_schedules, attributes below
+Required:
+    - data_factory_id
+    - name
+Optional:
+    - activated
+    - annotations
+    - description
+    - end_time
+    - frequency
+    - interval
+    - pipeline_name
+    - pipeline_parameters
+    - start_time
+    - time_zone
+    - pipeline (block):
+        - name (required)
+        - parameters (optional)
+    - schedule (block):
+        - days_of_month (optional)
+        - days_of_week (optional)
+        - hours (optional)
+        - minutes (optional)
+        - monthly (optional, block):
+            - week (optional)
+            - weekday (required)
+EOT
+
+  type = map(object({
+    data_factory_id     = string
+    name                = string
+    activated           = optional(bool, true)
+    annotations         = optional(list(string))
+    description         = optional(string)
+    end_time            = optional(string)
+    frequency           = optional(string, "Minute")
+    interval            = optional(number, 1)
+    pipeline_name       = optional(string)
+    pipeline_parameters = optional(map(string))
+    start_time          = optional(string)
+    time_zone           = optional(string)
+    pipeline = optional(object({
+      name       = string
+      parameters = optional(map(string))
+    }))
+    schedule = optional(object({
+      days_of_month = optional(list(number))
+      days_of_week  = optional(list(string))
+      hours         = optional(list(number))
+      minutes       = optional(list(number))
+      monthly = optional(object({
+        week    = optional(number)
+        weekday = string
+      }))
+    }))
+  }))
+}
+
